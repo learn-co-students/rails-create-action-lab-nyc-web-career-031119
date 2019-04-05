99d1641a41b31da748f2cad31fbd786cd65e54c9
github username <github email address>
2019-04-05 14:07:04 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,19 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
